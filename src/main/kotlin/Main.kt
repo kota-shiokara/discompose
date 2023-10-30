f7c6e0bb59473fd4acd863b42db18bf5710c4961
kota-shiokara <ikanoshiokara.fun@gmail.com>
2023-10-31 00:13:18 +0900

    stop実装できたんだが

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,17 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Slider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -22,8 +25,13 @@ import androidx.compose.ui.window.application
 import component.ChooseFileRow
 import korlibs.audio.sound.AudioStream
 import korlibs.audio.sound.PlaybackParameters
+import korlibs.audio.sound.PlaybackTimes
+import korlibs.audio.sound.SoundChannel
+import korlibs.audio.sound.await
 import korlibs.audio.sound.readAudioStream
+import korlibs.audio.sound.readSound
 import korlibs.audio.sound.toSound
+import korlibs.io.file.std.resourcesVfs
 import korlibs.io.file.std.toVfs
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -39,10 +47,18 @@ fun App() {
         val coroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Main + Dispatchers.IO)
         var filePath by remember { mutableStateOf("") }
         var dialogState by remember { mutableStateOf(false) }
-        var audioStream: AudioStream? by remember { mutableStateOf(null) }
         // 曲の時間
         var currentTime by remember { mutableStateOf(0f) }
         var totalTime by remember { mutableStateOf(0f) }
+
+        var soundChannel: SoundChannel? by remember { mutableStateOf(null) }
+
+        LaunchedEffect(soundChannel) {
+            soundChannel?.await { current, total ->
+                currentTime = current.seconds.toFloat()
+                totalTime = total.seconds.toFloat()
+            }
+        }
 
         Column(
             modifier = Modifier
@@ -54,32 +70,44 @@ fun App() {
                 filePath = it
             }
             Spacer(modifier = Modifier.padding(8.dp))
-            Button(
-                onClick = {
-                    println("Start")
-                    val file = File(filePath)
-                    coroutineScope.launch(Dispatchers.IO) {
-                        if (file.exists()) {
-                            audioStream = file.toVfs().readAudioStream()
-                            audioStream!!.toSound().playAndWait(
-                                PlaybackParameters(
-                                    volume = 0.2
+            Row {
+                Button(
+                    onClick = {
+                        println("Start")
+                        val file = File(filePath)
+                        coroutineScope.launch(Dispatchers.IO) {
+                            if (file.exists()) {
+                                soundChannel = file.toVfs().readSound().play(
+                                    PlaybackParameters(
+                                        volume = 0.2
+                                    )
                                 )
-                            ) { current, total ->
-                                currentTime = current.seconds.toFloat()
-                                totalTime = total.seconds.toFloat()
+                            } else {
+                                dialogState = true
                             }
-                        } else {
-                            dialogState = true
                         }
                     }
+                ) {
+                    Text("Play")
                 }
-            ) {
-                Text("Play")
+                Spacer(modifier = Modifier.size(8.dp))
+                Button(
+                    onClick = {
+                        println("Stop")
+                        if (soundChannel != null) {
+                            soundChannel!!.stop()
+                            soundChannel = null
+                            println("Stop Complete")
+                        }
+                    }
+                ) {
+                    Text("Stop")
+                }
             }
-
-            if (audioStream != null) {
-                Text("${(currentTime.toInt() / 2).formatTimer()} / ${(totalTime.toInt() / 2).formatTimer()}")
+            if (soundChannel != null) {
+                Text("${currentTime.toInt().formatTimer()} / ${totalTime.toInt().formatTimer()}")
+            } else {
+                Text("- / -")
             }
 
             Slider(
