c3f4e09493e9746e866fa502d64996b4662cd403
kota-shiokara <ikanoshiokara.fun@gmail.com>
2024-04-27 04:01:31 +0900

    small fix

--- src/main/kotlin/util/IntExt.kt
+++ src/main/kotlin/util/IntExt.kt
@@ -3,9 +3,8 @@ package util
 fun Int.formatTimer(): String {
     val seconds: Int = this % 60
     var minutes: Int = this / 60
-    var hour: Int = 0
     return if (minutes > 60) {
-        hour = minutes / 60
+        val hour = minutes / 60
         minutes %= 60
         String.format("%d:%02d:%02d", hour, minutes, seconds)
     } else {
